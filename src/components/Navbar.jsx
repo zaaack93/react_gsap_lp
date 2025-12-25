8d3eac81683ad13da2e66423652dee74caea6de5
zbouzangad <zakaria@mail.com>
2025-12-25 13:21:15 +0100

    feat: implement GSAP animations for Navbar background transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,24 @@
+import { useGSAP } from "@gsap/react"
 import { navLinks } from "../../constants"
+import gsap from "gsap"
 
 const Navbar = () => {
+    useGSAP(()=>{
+        const navTween =gsap.timeline({
+            scrollTrigger:{
+                trigger:"nav",
+                start: 'bottom top'
+            }
+        })
+
+        navTween.fromTo("nav", 
+            {background:'transparent'},
+            {background:'rgba(0,0,0,0.5)',backgroundFilter: 'blur(10px)', duration:1,ease:'power1.inOut'}
+        )
+
+    })
+
+
   return (
     <nav>
         <div>
