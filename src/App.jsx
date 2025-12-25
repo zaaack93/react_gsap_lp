6c6b93c0aeee89d22d3ca3741c8ca1a1281dd6ad
zbouzangad <zakaria@mail.com>
2025-12-25 12:17:55 +0100

    feat: integrate Tailwind CSS and GSAP for enhanced styling and animations
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Registered GSAP plugins (ScrollTrigger, SplitText) in App component.
    - Updated App component to use Tailwind CSS classes.
    - Created a comprehensive index.css file to include Tailwind CSS and custom styles.
    - Configured Vite to use Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='text-balance'>App</div>
   )
 }
 
