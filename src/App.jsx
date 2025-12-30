c7b033631825ba2daf7d002a9b36161ceec1cbab
zbouzangad <zakaria@mail.com>
2025-12-30 14:42:07 +0100

    add ART Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   )
 }
